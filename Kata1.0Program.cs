0e21c2bd6ac36c82116552d6b96d3632264e475b
cakaki68 <47216508+cakaki68@users.noreply.github.com>
2019-05-26 21:33:05 -0400

    Add files via upload

---
+++ Kata1.0Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Kata1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            class Main
+        {
+            public static void main(String[] args)
+            {
+                try
+                {
+
+
+                        Args args = new args("l,p#,d");
+                        boolean logging = arg.Getboolean("l");
+                        int port = arg.GetInt("p#");
+                        String directory = arg.GetString("d");
+                        Console.WriteLine("Arguments error");
+                }
+
+
+            }
+        }
+
+        //“l” (logging)
+        //“p” (port)
+        //“d” (directory) 
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace kata1._2
+{
+    class Program
+    {
+   
+            public static void main(String[] args)
+            {
+                try
+                {
+
+                    Args arg = new arg("l,p#,d*");
+                    boolean logging = arg.Getboolean("d*");
+                    int port = arg.Getint("p#");
+                    String Directory = arg.Getstring("d*");
+                    ExecuteApplication(logging, port, directory);
+
+                }
+    }
+}
